82083ad6cafd6be78d344b8517f67110491ededf
Facundo Mazzola <facumazzola2@gmail.com>
2025-06-27 19:43:13 +0000

    added class DTypeMapper to map pandas dtypes to MySQL types

--- connectors/MySQL.py
+++ connectors/MySQL.py
@@ -14,7 +14,8 @@ class MySQLConnector:
                 host=self.host,
                 user=self.user,
                 password=self.password,
-                database=self.database
+                database=self.database,
+                port=self.port
             )
             if connection.is_connected():
                 print("Connection successful")
@@ -26,17 +27,80 @@ class MySQLConnector:
             if connection and connection.is_connected():
                 connection.close()
 
-    def get_data(self,query):
+    def connect(self):
         import mysql.connector
-        import pandas as pd
-        connection = mysql.connector.connect(
+        self.connection = mysql.connector.connect(
             host=self.host,
             user=self.user,
             password=self.password,
-            database=self.database
+            database=self.database,
+            port=self.port
         )
+        self.cursor = self.connection.cursor()
+        return self
 
-        cursor = connection.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return pd.DataFrame(result, columns=[i[0] for i in cursor.description])
+    def get_data(self,query):
+        import mysql.connector
+        import pandas as pd
+        try:
+            self.cursor.execute(query)
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return pd.DataFrame()
+
+        result = self.cursor.fetchall()
+        return pd.DataFrame(result, columns=[i[0] for i in self.cursor.description])
+    
+    def check_existance(self, type,name):
+        self.cursor.execute(f"SHOW {type} LIKE '{name}'")
+        return self.cursor.fetchone() is not None
+
+    def insert_data(self,data,table_name):
+        # check table existance
+        if self.check_existance('TABLE', table_name):
+            pass
+        else:
+            q = f"""
+            CREATE TABLE IF NOT EXISTS {table_name}
+            """
+
+    def close(self):
+        if self.connection and self.connection.is_connected():
+            self.cursor.close()
+            self.connection.close()
+            print("Connection closed")
+        else:
+            print("No active connection to close")
+        return self
+
+
+class DTypeMapper:
+    def __init__(self):
+        self.map_dict = {}
+
+    def map(self,data):
+        import pandas as pd
+        dtypes = data.dtypes
+        for col, dtype in dtypes.items():
+            if pd.api.types.is_integer_dtype(dtype):
+                self.map_dict[col] = 'INT'
+            elif pd.api.types.is_float_dtype(dtype):
+                self.map_dict[col] = 'FLOAT'
+            elif pd.api.types.is_string_dtype(dtype):
+                self.map_dict[col] = 'VARCHAR(255)'
+            elif pd.api.types.is_datetime64_any_dtype(dtype):
+                self.map_dict[col] = 'DATETIME'
+            elif data[col].dtype == 'boolean':
+                self.map_dict[col] = 'BOOLEAN'
+            else:
+                self.map_dict[col] = 'TEXT'
+        return self.map_dict
+    
+    def __str__(self):
+        s = "Data Type Mapping:\n"
+        mk = max(len(k) for k in self.map_dict.keys())
+        for col, dtype in self.map_dict.items():
+            s += f"{col.ljust(mk)}: {dtype}\n"
+        return s
+
+            
