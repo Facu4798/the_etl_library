4800fa2a11864effc4510781e242309b8fecee0d
Facundo Mazzola <facumazzola2@gmail.com>
2025-06-28 00:05:43 +0000

    updated credentials to have a dictionary representation

--- authentication.py
+++ authentication.py
@@ -2,16 +2,18 @@ class Credentials:
     def __init__(self,params):
         for key, value in params.items():
             setattr(self, key, value)
+        self.dict = params
 
     def add_credential(self, key, value):
         setattr(self, key, value)
+        self.dict[key] = value
 
     def show(self):
         print(self.__str__())
+
     def __str__(self):
-        s = self.__dict__
-        mk = max(len(k) for k in s.keys())
+        mk = max(len(k) for k in self.dict.keys())
         s2 = "Credentials:\n"
-        for k, v in s.items():
+        for k, v in self.dict.items():
             s2 += f"{k.ljust(mk+2)} = {v}\n"
         return s2
