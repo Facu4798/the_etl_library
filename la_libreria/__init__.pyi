570a8bcb37f6536974ef2c41f1670d16036e5b4e
Facundo Mazzola <facumazzola2@gmail.com>
2025-08-23 19:58:00 -0300

    Import get_date and get_ts from utils

--- la_libreria/__init__.pyi
+++ la_libreria/__init__.pyi
@@ -2,3 +2,5 @@ from .authentication import Credentials
 from .connectors import MySQLConnector
 from .base import BaseActivity
 from .enviroment import Env
+from .utils import get_date
+from .utils import get_ts
