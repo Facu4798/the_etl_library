409d8e8c78292297090b37a5315ba09b7637299b
Facundo Mazzola <facumazzola2@gmail.com>
2025-08-23 23:22:35 -0300

    Implement date subtraction function
    Add a function to subtract a specified interval from a date.

--- la_libreria/utils.py
+++ la_libreria/utils.py
@@ -5,3 +5,14 @@ def get_date():
 def get_ts():
     import datetime
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def substract_date(date_str,interval="d",amount=1, date_format="%Y-%m-%d"):
+    from datetime import datetime, timedelta
+    date_obj = datetime.strptime(s, date_format)
+    if interval == "d":
+        prev = date_obj - timedelta(days=amount)
+    if interval == "m":
+        prev = date_obj - timedelta(months=amount)
+    if intervsal == "y:
+        prev = date_obj - timedelta(years=amount)
+    return prev.strftime(date_format)
